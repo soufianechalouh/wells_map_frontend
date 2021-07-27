2ae492661d54ac1265a9286356876b1f623b2e1b
Soufiane CHALOUH <soufianechalouh@gmail.com>
2021-07-27 09:20:47 +0100

    feat: Call data from API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
+import {Component} from "react";
 import WellsMap from "./components/WellsMap";
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: `http://localhost:5000`
+})
+
+class App extends Component {
+
+    state = {
+        wellsData: {}
+    }
+    constructor() {
+        super();
+        api.get("/data").then(res => {
+            console.log(res.data["data"])
+            this.setState( {wellsData : res.data["data"]})
+        })
+    }
 
 
-function App() {
-  return (
-    <div>
-        <div id="mapid">
-            <WellsMap/>
-        </div>
-    </div>
-  );
+    render(){
+      return (
+            <div>
+                <div id="mapid">
+                    {Object.keys(this.state.wellsData).length > 0 ? <WellsMap wellsData={this.state.wellsData}/>: "Wait a bit, please"}
+                </div>
+            </div>
+          );
+    }
 }
 
 export default App;

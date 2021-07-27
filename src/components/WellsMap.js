28eafa0c2a5278b2949f48bc6f12c46c5cb8a8ca
Soufiane CHALOUH <soufianechalouh@gmail.com>
2021-07-27 09:23:47 +0100

    feat: implement Markers Clusters

--- src/components/WellsMap.js
+++ src/components/WellsMap.js
@@ -1,37 +1,36 @@
 import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
-import * as wellsData from "../data/data.json";
+import MarkerClusterGroup from 'react-leaflet-cluster'
 
-//TODO: Fix issue of markers not showing up
-const WellsMap = () => {
-    const position = [45.4, -75.7]
+const WellsMap = ({ wellsData }) =>{
+
+    const position = [40.1, -100.1]
 
     return (
-        <MapContainer center={position} zoom={13} style={{height: "1000px"}} scrollWheelZoom={false}>
+        <div>
+        <MapContainer center={position} zoom={5} style={{height: "1000px"}} scrollWheelZoom={false}>
             <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+            <MarkerClusterGroup>
 
-            {wellsData.features.map(well => (
+            {Object.keys(wellsData).length > 0 ? wellsData.features.map(well => (
                     <Marker
-                        key={well.properties.PARK_ID}
+                        key={well.properties.PNAME}
                         position={[
                             well.geometry.coordinates[1],
                             well.geometry.coordinates[0]
                         ]}>
                         <Popup>
-                            A pretty CSS3 {well.properties.PARK_ID}. <br/> Easily customizable.
+                            {well.properties.PNAME} - ({well.properties.PSTATABB}) <br/> Easily customizable.
                         </Popup>
                     </Marker>
                 )
-            )}
-            <Marker position={position}>
-                <Popup>
-                    A pretty CSS3 popup. <br/> Easily customizable.
-                </Popup>
-            </Marker>
+            ) : ""}
+            </MarkerClusterGroup>
         </MapContainer>
+            </div>
     )
 }
 
